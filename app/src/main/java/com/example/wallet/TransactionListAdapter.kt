46777c98adc5d9c9dbf07b19cf03adbb67ae2de8
Eunice Atieno <eunniceatieno@gmail.com>
2021-08-26 18:59:21 +0300

    ..

--- app/src/main/java/com/example/wallet/TransactionListAdapter.kt
+++ app/src/main/java/com/example/wallet/TransactionListAdapter.kt
@@ -6,5 +6,33 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class TransactionListAdapter {
+
+class TransactionListAdapter(var transContent: List<Wallet>):RecyclerView.Adapter<TransactionsViewHolderr>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionsViewHolder {
+        var itemView= LayoutInflater.from(parent.context).inflate(R.layout.transactionviewlist, parent,false)
+        return TransactionsViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: TransactionsViewHolder, position: Int) {
+        var currentTrans=transContent[position]
+        holder.tvDes.text=currentTrans.description
+        holder.tvRef.text=currentTrans.reference
+        holder.tvAmount.text=currentTrans.amount.toString()
+        holder.tvDate.text=currentTrans.date
+        holder.tvCard.text=currentTrans.card
+
+    }
+
+    override fun getItemCount(): Int {
+        return transContent.size
+    }
+}
+class TransactionsViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+    var tvDes=itemView.findViewById<TextView>(R.id.tvDes)
+    var tvRef=itemView.findViewById<TextView>(R.id.tvRef)
+    var tvAmount=itemView.findViewById<TextView>(R.id.tvAmount)
+    var tvDate=itemView.findViewById<TextView>(R.id.tvDate)
+    var tvCard=itemView.findViewById<TextView>(R.id.tvCard)
+
+
 }
